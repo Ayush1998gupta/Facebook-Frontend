ef994792756f4b9d2f576ce7d51fe3c31a2ce5b1
Ayush <aayush199813@gmail.com>
2022-09-01 17:33:40 +0530

    react routr dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-function App() {
-  const get = async () => {
-    const res = await fetch('http//localhost:8000');
-    console.log(res);
-  };
-  get();
-  return <div>welcome to frontend</div>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
