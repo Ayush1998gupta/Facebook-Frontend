5eace2a013ecfaa6a134bd6d90dddf61eeaf6537
Ayush <aayush199813@gmail.com>
2022-09-04 16:19:07 +0530

    Register UI

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,13 +2,14 @@ import './style.css';
 
 import LoginForm from '../../components/login/LoginForm';
 import Footer from '../../components/login/Footer';
+import RegisterForm from '../../components/login/RegisterForm';
 
 const Login = () => {
   return (
     <div className="login">
       <div className="login_wrapper">
         <LoginForm />
-        <div className="register"></div>
+        <RegisterForm />
         <Footer />
       </div>
     </div>
